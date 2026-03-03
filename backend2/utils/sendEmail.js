3787a150940b82672201b3d1cbb8161b89e864af
Ujjwal Mutneja <ujjwal2478@gmail.com>
2026-03-03 21:25:25 +0530

    email functionality correction

--- backend2/utils/sendEmail.js
+++ backend2/utils/sendEmail.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (subject, htmlContent) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port:465,
+    secure: true,
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_APP_PASSWORD,
